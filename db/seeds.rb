d4a49067e1808e0388c1df6ca1e442c8bd126e22
wwinslowkent <wwinslowkent@gmail.com>
2017-03-06 12:23:03 -0500

    bug fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,43 +11,53 @@ data = CSV.read("factual_data - factual_data.csv")
 data.each do |row|
   r = Restaurant.create(address: row[0], latutude: row[1], longitude: row[2], name: row[3], price: row[4], rating: row[5])
   if row[6] != ''
-    if Category.exists?(:name => row[6])
+    if Category.exists?(name: row[6])
       cat = Category.find_by(name: row[6])
       Restcat.create(restaurant_id: r.id, category: cat)
     else
       Category.create(name: row[6])
+      cat = Category.find_by(name: row[6])
+      Restcat.create(restaurant_id: r.id, category: cat)
     end
   end
   if row[7] != ''
-    if Category.exists?(:name => row[7])
+    if Category.exists?(name: row[7])
       cat = Category.find_by(name: row[7])
       Restcat.create(restaurant_id: r.id, category: cat)
     else
-      Category.create(name: row[6])
+      Category.create(name: row[7])
+      cat = Category.find_by(name: row[7])
+      Restcat.create(restaurant_id: r.id, category: cat)
     end
   end
   if row[8] != ''
-    if Category.exists?(:name => row[8])
+    if Category.exists?(name: row[8])
       cat = Category.find_by(name: row[8])
       Restcat.create(restaurant_id: r.id, category: cat)
     else
-      Category.create(name: row[6])
+      Category.create(name: row[8])
+      cat = Category.find_by(name: row[8])
+      Restcat.create(restaurant_id: r.id, category: cat)
     end
   end
   if row[9] != ''
-    if Category.exists?(:name => row[9])
+    if Category.exists?(name: row[9])
       cat = Category.find_by(name: row[9])
       Restcat.create(restaurant_id: r.id, category: cat)
     else
-      Category.create(name: row[6])
+      Category.create(name: row[9])
+      cat = Category.find_by(name: row[9])
+      Restcat.create(restaurant_id: r.id, category: cat)
     end
   end
   if row[10] != ''
-    if Category.exists?(:name => row[10])
+    if Category.exists?(name: row[10])
       cat = Category.find_by(name: row[10])
       Restcat.create(restaurant_id: r.id, category: cat)
     else
-      Category.create(name: row[6])
+      Category.create(name: row[10])
+      cat = Category.find_by(name: row[10])
+      Restcat.create(restaurant_id: r.id, category: cat)
     end
   end
 end
