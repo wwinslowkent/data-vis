bb4acc9cd3945bea9bf34e0a7c7ef3d8c73728ca
wwinslowkent <wwinslowkent@gmail.com>
2017-03-16 16:24:57 -0400

    set up view and controller for graph

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homepage#index'
-
-    resources :restaurants
+  resources :restaurants
+  get 'graph/index'
+  get 'graph/data', :defaults => { :format => 'json' }
 
 end
